c3e68b6caacd43b260bf6b4b4c1418e0ca5f7146
FATIZ-CH <78651355+FATIZ-CH@users.noreply.github.com>
2021-03-30 13:20:02 +0100

    Create 1-neuron.py

---
+++ 0x01-classification/1-neuron.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import numpy as np
+class Neuron:
+    def __init__(self, nx):
+        '''INTEGER CONDITION'''
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        elif nx < 1:
+            raise ValueError('nx must be a positive integer')
+        else:
+            self.W = np.random.normal(size=(1, nx))
+            self.b = 0
+            self.A = 0
+             
+    @property
+    def W(self):
+        """Returns: private instance weight"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Returns: private instance bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Returns: private instance output"""
+        return self.__A
