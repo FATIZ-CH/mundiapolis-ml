d33bb96215025f5807ac4086951a00877dee51f8
FATIZ-CH <78651355+FATIZ-CH@users.noreply.github.com>
2021-04-01 20:31:04 +0100

    Create 17-deep_neural_network.py

---
+++ 0x01-classification/17-deep_neural_network.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+Module contenant la classe neurone
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    classification binaire
+    """
+
+    def __init__(self, nx, layers):
+        """
+        constructeur de classe
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        weights = {}
+        for i in range(len(layers)):
+            if layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            key_w = 'W' + str(i + 1)
+            key_b = 'b' + str(i + 1)
+            if i == 0:
+                weights[key_w] = np.random.randn(layers[i], nx)*np.sqrt(2 / nx)
+            else:
+                weights[key_w] = np.random.randn(layers[i], layers[
+                    i-1]) * np.sqrt(2 / layers[i-1])
+            weights[key_b] = np.zeros((layers[i], 1))
+        self.__weights = weights
+
+    @property
+    def cache(self):
+        """
+        fonction getter pour le cache
+        """
+        return self.__cache
+
+    @property
+    def L(self):
+        """
+        fonction getter pour L (nombre de couches)
+        """
+        return self.__L
+
+    @property
+    def weights(self):
+        """
+        fonction getter pour weights
+        """
+        return self.__weights
